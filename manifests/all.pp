3ac4dc51bf771b5ca661b20a74e57a449b1a2766
Dan Bode <dan@puppetlabs.com>
2011-06-18 23:54:29 -0700

    moved nova all test to manifest.

---
+++ manifests/all.pp
@@ -0,0 +1,27 @@
+#
+# This manifest installs all of the nova
+# components on one node.
+#
+resources { 'nova_config':
+  purge => true,
+}
+class { 'mysql::server': }
+class { 'nova::all':
+  db_password => 'password',
+  db_name => 'nova',
+  db_user => 'nova',
+  db_host => 'localhost',
+
+  rabbit_password => 'rabbitpassword',
+  rabbit_port => '5672',
+  rabbit_userid => 'rabbit_user',
+  rabbit_virtual_host => '/',
+  rabbit_host => 'localhost',
+
+  image_service => 'nova.image.glance.GlanceImageService',
+
+  glance_host => 'localhost',
+  glance_port => '9292',
+
+  libvirt_type => 'qemu',
+}
