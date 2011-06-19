8fd04c8adb37c4a54314090fafed36c307454dfb
Dan Bode <dan@puppetlabs.com>
2011-06-18 23:23:07 -0700

    Fixed bug in test script.

--- ext/test.sh
+++ ext/test.sh
@@ -4,9 +4,12 @@ unzip nova.zip
 source novarc
 glance add name=ramdisk disk_format=ari container_format=ari is_public=True < /vagrant/images/lucid_ami/initrd.img-2.6.32-23-server
 glance add name=kernel disk_format=aki container_format=aki is_public=True < /vagrant/images/lucid_ami/vmlinuz-2.6.32-23-server
-glance add name=lucid_ami disk_format=ami container_format=ami is_public=True ramdisk_id=1 kernel_id=2 < ubuntu-lucid.img
+glance add name=lucid_ami disk_format=ami container_format=ami is_public=True ramdisk_id=1 kernel_id=2 < /vagrant/images/lucid_ami/ubuntu-lucid.img
 glance index
 nova flavor-list
 nova image-list
 nova boot test --flavor 1 --image 3
 euca-describe-instances
+echo 'check the status of your VM with euca-describe-instances'
+echo 'when it is in the running state, verigy that you can login'
+
