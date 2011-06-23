75851ad65a768ffbf2789eb059c41972412072cb
Dan Bode <dan@puppetlabs.com>
2011-06-22 19:09:43 -0700

    Added stuff related to my testing of multi-node

--- manifests/hosts.pp
+++ manifests/hosts.pp
@@ -5,4 +5,18 @@ host { 'puppetmaster':
 host { 'all':
   ip => '172.20.0.11',
 }
-
+host { 'db':
+  ip => '172.20.0.12'
+}
+host { 'rabbitmq':
+  ip => '172.20.0.13',
+}
+host { 'controller':
+  ip => '172.20.0.14',
+}
+host { 'compute':
+  ip => '172.20.0.15',
+}
+host { 'glance':
+  ip => '172.20.0.16',
+}
---
+++ scripts/run-glance.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+bash /vagrant/scripts/run.sh glance
