3a3940d7647852dab89b58d0196faf65ebbd7bbd
Branan Purvine-Riley <branan@puppetlabs.com>
2012-06-12 11:35:07 -0700

    Use i386 instead of x86_64 cirros image in the test script
    This makes it possible to run the test setup on a wider range of machines.
    Especially when testing in a VM there's a chance that the user has a 32-bit
    device, and the 64-bit cirros image will fail to boot in such a case

--- files/nova_test.sh
+++ files/nova_test.sh
@@ -12,9 +12,9 @@ source /root/openrc
 
 #IMAGE_ID=`glance index | grep 'Ubuntu 11.10 cloudimg amd64' | head -1 |  awk -F' ' '{print $1}'`
 
-wget https://launchpad.net/cirros/trunk/0.3.0/+download/cirros-0.3.0-x86_64-disk.img
+wget https://launchpad.net/cirros/trunk/0.3.0/+download/cirros-0.3.0-i386-disk.img
 
-glance add name='cirros image' is_public=true container_format=bare disk_format=qcow2 < cirros-0.3.0-x86_64-disk.img
+glance add name='cirros image' is_public=true container_format=bare disk_format=qcow2 < cirros-0.3.0-i386-disk.img
 
 IMAGE_ID=`glance index | grep 'cirros image' | head -1 |  awk -F' ' '{print $1}'`
 
