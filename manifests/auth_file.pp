16cedd9642e8cf20c85c72137ddfadcb17b4d204
Dan Bode <dan@puppetlabs.com>
2012-08-20 04:16:04 -0700

    Remove unused public controller node.
    This commit removes the unused parameter
    controller node.

--- manifests/auth_file.pp
+++ manifests/auth_file.pp
@@ -5,8 +5,7 @@
 #
 class openstack::auth_file(
   $admin_password,
-  $public_address,
-  $controller_node      = '127.0.0.1',
+  $public_address       = '127.0.0.1',
   $keystone_admin_token = 'keystone_admin_token',
   $admin_user           = 'admin',
   $admin_tenant         = 'admin'
