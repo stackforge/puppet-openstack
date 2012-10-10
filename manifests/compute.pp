49173c6f403f254065ce21545389afffd1a96b3a
Dan Bode <dan@puppetlabs.com>
2012-10-09 22:37:31 -0700

    total refactor of openstack::compute

--- manifests/compute.pp
+++ manifests/compute.pp
@@ -1,93 +1,148 @@
 #
-# This class is intended to serve as
-# a way of deploying compute nodes.
+# == Class: openstack::compute
 #
-# This currently makes the following assumptions:
-#   - libvirt is used to manage the hypervisors
-#   - flatdhcp networking is used
-#   - glance is used as the backend for the image service
+# Manifest to install/configure nova-compute and nova-volume
 #
-# [private_interface] Interface used for vm networking connectivity. Required.
-# [internal_address] Internal address used for management. Required.
-# [public_interface] Public interface used to route public traffic. Optional.
-#   Defaults to false.
-# [fixed_range] Range of ipv4 network for vms.
-# [network_manager] Nova network manager to use.
-# [multi_host] Rather node should support multi-host networking mode for HA.
-#   Optional. Defaults to false.
-# [network_config] Hash that can be used to pass implementation specifc
-#   network settings. Optioal. Defaults to {}
-# [sql_connection] SQL connection information. Optional. Defaults to false
-#   which indicates that exported resources will be used to determine connection
-#   information.
-# [nova_user_password] Nova service password.
-#  [rabbit_host] RabbitMQ host. False indicates it should be collected.
-#    Optional. Defaults to false,
-#  [rabbit_password] RabbitMQ password. Optional. Defaults to  'rabbit_pw',
-#  [rabbit_user] RabbitMQ user. Optional. Defaults to 'nova',
-#  [glance_api_servers] List of glance api servers of the form HOST:PORT
-#    delimited by ':'. False indicates that the resource should be collected.
-#    Optional. Defaults to false,
-#  [libvirt_type] Underlying libvirt supported hypervisor.
-#    Optional. Defaults to 'kvm',
-#  [vncproxy_host] Host that serves as vnc proxy. Optional.
-#    Defaults to false. False indicates that a vnc proxy should not be configured.
-#  [vnc_enabled] Rather vnc console should be enabled.
-#    Optional. Defaults to 'true',
-#  [verbose] Rather components should log verbosely.
-#    Optional. Defaults to false.
-#  [manage_volumes] Rather nova-volume should be enabled on this compute node.
-#    Optional. Defaults to false.
-#  [nova_volumes] Name of volume group in which nova-volume will create logical volumes.
-#    Optional. Defaults to nova-volumes.
+# === Parameters
 #
-class openstack::compute(
-  $private_interface,
+# See params.pp
+#
+# === Examples
+#
+# class { 'openstack::nova::compute':
+#   internal_address   => '192.168.2.2',
+#   vncproxy_host      => '192.168.1.1',
+#   nova_user_password => 'changeme',
+# }
+
+class openstack::compute (
+  # Required Network
   $internal_address,
-  # networking config
-  $public_interface    = undef,
-  $fixed_range         = '10.0.0.0/16',
-  $network_manager     = 'nova.network.manager.FlatDHCPManager',
-  $multi_host          = false,
-  $network_config      = {},
-  # my address
-  # conection information
-  $sql_connection      = false,
-  $nova_user_password  = 'nova_pass',
-  $rabbit_host         = false,
-  $rabbit_password     = 'rabbit_pw',
-  $rabbit_user         = 'nova',
-  $glance_api_servers  = false,
-  # nova compute configuration parameters
-  $libvirt_type        = 'kvm',
-  $vncproxy_host       = false,
-  $vnc_enabled         = 'true',
-  $verbose             = false,
-  $manage_volumes      = false,
-  $nova_volume         = 'nova-volumes'
+  # Required Nova
+  $nova_user_password,
+  # Required Rabbit
+  $rabbit_password,
+  # Network
+  $public_address                = undef,
+  $public_interface              = undef,
+  $private_interface             = 'eth1',
+  $fixed_range                   = '10.0.0.0/24',
+  $network_manager               = 'nova.network.manager.FlatDHCPManager',
+  $network_config                = {},
+  $multi_host                    = false,
+  # DB
+  $sql_connection                = false,
+  # Nova
+  $purge_nova_config              = true,
+  # Rabbit
+  $rabbit_host                   = false,
+  $rabbit_user                   = 'nova',
+  # Glance
+  $glance_api_servers            = false,
+  # Virtualization
+  $libvirt_type                  = 'kvm',
+  # Volumes
+  $nova_volume                   = 'nova-volumes',
+  $manage_volumes                = true,
+  # TODO - not sure if using a variable as a default really works
+  $iscsi_ip_address              = $internal_address,
+  # VNC
+  $vnc_enabled                   = true,
+  $vncproxy_host                 = undef,
+  # General
+  $verbose                       = 'False',
+  $enabled                       = true
 ) {
 
-  warning('This class will be deprecated in favor of openstack::nova::compute')
-  class { 'openstack::nova::compute':
-    private_interface  => $private_interface,
-    internal_address   => $internal_address,
-    public_interface   => $public_interface,
-    fixed_range        => $fixed_range,
-    network_manager    => $network_manager,
-    multi_host         => $multi_host,
-    network_config     => $network_config,
+  #
+  # indicates that all nova config entries that we did
+  # not specifify in Puppet should be purged from file
+  #
+  if ! defined( Resources[nova_config] ) {
+    if ($purge_nova_config) {
+      resources { 'nova_config':
+        purge => true,
+      }
+    }
+  }
+
+  $final_sql_connection = $sql_connection
+  $glance_connection = $glance_api_servers
+  $rabbit_connection = $rabbit_host
+
+  class { 'nova':
     sql_connection     => $sql_connection,
-    nova_user_password => $nova_user_password,
-    rabbit_host        => $rabbit_host,
+    rabbit_userid      => $rabbit_user,
     rabbit_password    => $rabbit_password,
-    rabbit_user        => $rabbit_user,
+    image_service      => 'nova.image.glance.GlanceImageService',
     glance_api_servers => $glance_api_servers,
-    libvirt_type       => $libvirt_type,
-    vncproxy_host      => $vncproxy_host,
-    vnc_enabled        => $vnc_enabled,
     verbose            => $verbose,
-    manage_volumes     => $manage_volumes,
-    nova_volume        => $nova_volume,
+    rabbit_host        => $rabbit_host,
+  }
+
+  # Install / configure nova-compute
+  class { '::nova::compute':
+    enabled                       => $enabled,
+    vnc_enabled                   => $vnc_enabled,
+    vncserver_proxyclient_address => $internal_address,
+    vncproxy_host                 => $vncproxy_host,
+  }
+
+  # Configure libvirt for nova-compute
+  class { 'nova::compute::libvirt':
+    libvirt_type     => $libvirt_type,
+    vncserver_listen => $internal_address,
+  }
+
+  # if the compute node should be configured as a multi-host
+  # compute installation
+  if $multi_host {
+    include keystone::python
+    nova_config {
+      'multi_host':      value => 'True';
+      'send_arp_for_ha': value => 'True';
+    }
+    if ! $public_interface {
+      fail('public_interface must be defined for multi host compute nodes')
+    }
+    $enable_network_service = true
+    class { 'nova::api':
+      enabled           => true,
+      admin_tenant_name => 'services',
+      admin_user        => 'nova',
+      admin_password    => $nova_user_password,
+    }
+  } else {
+    $enable_network_service = false
+    nova_config {
+      'multi_host':      value => 'False';
+      'send_arp_for_ha': value => 'False';
+    }
+  }
+
+  class { 'nova::network':
+    private_interface => $private_interface,
+    public_interface  => $public_interface,
+    fixed_range       => $fixed_range,
+    floating_range    => false,
+    network_manager   => $network_manager,
+    config_overrides  => $network_config,
+    create_networks   => false,
+    enabled           => $enable_network_service,
+    install_service   => $enable_network_service,
+  }
+
+  if $manage_volumes {
+    # Install / configure nova-volume
+    class { 'nova::volume':
+      enabled => $enabled,
+    }
+    if $enabled {
+      class { 'nova::volume::iscsi':
+        volume_group     => $nova_volume,
+        iscsi_ip_address => $iscsi_ip_address,
+      }
+    }
   }
 
 }
