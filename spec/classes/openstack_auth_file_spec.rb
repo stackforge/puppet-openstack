169ddcc8d5d21d4afc424190da675193d7f5d690
newptone <yuxcer@gmail.com>
2013-04-17 22:40:33 +0800

    Add param use_no_cache to the openstack::auth_file
    Fix bug 1169993
    
       This patch contains:
          *  use_no_cache param will add BooleanVar OS_NO_CACHE to the openrc,
             in the default its value is true.
          *  add the openstack_auth_file_spec for test.
    
    Change-Id: I3885bccb4c06bbe652f7fee1d5db83913d8cd79b

---
+++ spec/classes/openstack_auth_file_spec.rb
@@ -0,0 +1,65 @@
+require 'spec_helper'
+
+describe 'openstack::auth_file' do
+
+  let :default_params do
+    {
+      :admin_password        => 'admin',
+      :controller_node       => '127.0.0.1',
+      :keystone_admin_token  => 'keystone_admin_token',
+      :admin_user            => 'admin',
+      :admin_tenant          => 'admin',
+      :use_no_cache          => 'true',
+    }
+  end
+
+
+  describe "when using default class parameters" do
+    let :params do
+      default_params
+    end
+    it 'should create a openrc file' do
+      contain_file('/root/openrc').with_content(
+          '
+          export OS_NO_CACHE=true
+          export OS_TENANT_NAME=admin
+          export OS_USERNAME=admin
+          export OS_PASSWORD=admin
+          export OS_AUTH_URL="http://127.0.0.1:5000/v2.0/"
+          export OS_AUTH_STRATEGY=keystone
+          export SERVICE_TOKEN=keystone_admin_token
+          export SERVICE_ENDPOINT=http://127.0.0.1:35357/v2.0/
+          '
+        )
+    end
+  end
+
+  describe 'when overridding' do
+
+      let :params do
+          default_params.merge({
+            :admin_password        => 'nova',
+            :controller_node       => '127.0.0.2',
+            :keystone_admin_token  => 'keystone',
+            :admin_user            => 'nova',
+            :admin_tenant          => 'nova',
+            :use_no_cache          => 'false',
+          })
+      end
+
+      it 'should create a openrc file' do
+        contain_file('/root/openrc').with_content(
+          '
+          export OS_NO_CACHE=false
+          export OS_TENANT_NAME=nova
+          export OS_USERNAME=nova
+          export OS_PASSWORD=nova
+          export OS_AUTH_URL="http://127.0.0.2:5000/v2.0/"
+          export OS_AUTH_STRATEGY=keystone
+          export SERVICE_TOKEN=keystone
+          export SERVICE_ENDPOINT=http://127.0.0.2:35357/v2.0/
+          '
+        )
+      end
+  end
+end
