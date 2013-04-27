ab5b9055281a411ea2691299bd69fb2caf56a7c0
newptone <yuxcer@gmail.com>
2013-04-27 14:52:21 +0800

    Rename the param controller_node_address
        The param controller_node_address in openstack::swift::proxy is
        puzzling, b/c it actually means the keystone host address,but the
        keystone_host is not always equivalent to controller_host.
        This patch rename it to the keystone_host.
    
    Change-Id: Ia58d256370b3fa94c9f1010245f6a20395b668f1

--- manifests/swift/proxy.pp
+++ manifests/swift/proxy.pp
@@ -16,11 +16,11 @@ class openstack::swift::proxy (
   $ratelimit_rate_buffer_seconds    = 5,
   $ratelimit_account_ratelimit      = 0,
   $package_ensure                   = 'present',
-  $controller_node_address          = '10.0.0.1',
+  $keystone_host                    = '10.0.0.1',
   $memcached                        = true
 ) {
 
-  class { 'swift': 
+  class { 'swift':
     swift_hash_suffix => $swift_hash_suffix,
     package_ensure    => $package_ensure,
   }
@@ -59,7 +59,7 @@ class openstack::swift::proxy (
   }
 
   class { '::swift::proxy::s3token':
-    auth_host     => $controller_node_address,
+    auth_host     => $keystone_host,
     auth_port     => '35357',
   }
   class { '::swift::proxy::keystone':
@@ -69,7 +69,7 @@ class openstack::swift::proxy (
     admin_user        => 'swift',
     admin_tenant_name => 'services',
     admin_password    => $swift_user_password,
-    auth_host         => $controller_node_address,
+    auth_host         => $keystone_host,
   }
 
   # collect all of the resources that are needed
