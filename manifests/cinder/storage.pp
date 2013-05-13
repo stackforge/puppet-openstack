1277c26bb4b48cd5c0044dadae20d48b218ce0a8
Dan Bode <bodepd@gmail.com>
2013-05-13 13:15:12 -0700

    refactor of cinder manifests
    This commit refactors the cinder manifests to make
    them more consistent with other manifests:
    
    * removed default value for keystone_password
    * removed the prefix cinder_ from class parameters
    (it is redundant)
    * set package_ensure defaults to present
    * removed extra package_ensure parameter
    * changed $iscsi_enabled to $volume_driver (this
    change is being done in anticipation of needing
    additional volume drivers)
    * add test coverage
    * simple updates to .gitignore
    
    It also delete the openstack::cinder class b/c
    I could not seem to declare ::cinder while it
    existed.
    
    Change-Id: I4a7b49d95957675be82c3b77958ae9d0c47eb4fa

--- manifests/cinder/storage.pp
+++ manifests/cinder/storage.pp
@@ -1,48 +1,50 @@
 class openstack::cinder::storage(
   $sql_connection,
   $rabbit_password,
-  $rabbit_userid                  = 'guest',
-  $rabbit_host                    = '127.0.0.1',
-  $rabbit_hosts                   = undef,
-  $rabbit_port                    = '5672',
-  $rabbit_virtual_host            = '/',
-  $cinder_package_ensure          = 'present',
-  $api_paste_config               = '/etc/cinder/api-paste.ini',
-  $cinder_package_ensure          = 'latest',
-  $cinder_volume_package_ensure   = 'latest',
-  $volume_group                   = 'cinder-volumes',
-  $cinder_volume_enabled          = true,
-  $iscsi_enabled                  = true,
-  $iscsi_ip_address               = '127.0.0.1',
-  $setup_test_volume              = true,
-  $cinder_verbose                 = 'False',
+  $rabbit_userid         = 'guest',
+  $rabbit_host           = '127.0.0.1',
+  $rabbit_hosts          = undef,
+  $rabbit_port           = '5672',
+  $rabbit_virtual_host   = '/',
+  $package_ensure        = 'present',
+  $api_paste_config      = '/etc/cinder/api-paste.ini',
+  $volume_package_ensure = 'present',
+  $volume_group          = 'cinder-volumes',
+  $enabled               = true,
+  $volume_driver         = 'iscsi',
+  $iscsi_ip_address      = '127.0.0.1',
+  $setup_test_volume     = false,
+  $verbose               = false
 ) {
 
   class {'::cinder':
-    sql_connection      => $cinder_sql_connection,
+    sql_connection      => $sql_connection,
     rabbit_userid       => $rabbit_userid,
     rabbit_password     => $rabbit_password,
     rabbit_host         => $rabbit_host,
     rabbit_port         => $rabbit_port,
     rabbit_hosts        => $rabbit_hosts,
     rabbit_virtual_host => $rabbit_virtual_host,
-    package_ensure      => $cinder_package_ensure,
-    api_paste_config    => $cinder_api_paste_config,
-    verbose             => $cinder_verbose,
+    package_ensure      => $package_ensure,
+    api_paste_config    => $api_paste_config,
+    verbose             => $verbose,
   }
 
 
   class { '::cinder::volume':
-    package_ensure          => $cinder_volume_package_ensure,
-    enabled                 => $cinder_volume_enabled,
+    package_ensure => $volume_package_ensure,
+    enabled        => $enabled,
   }
-  if $iscsi_enabled {
-    class { '::cinder::volume::iscsi':
+
+  if $volume_driver == 'iscsi' {
+    class { 'cinder::volume::iscsi':
       iscsi_ip_address => $iscsi_ip_address,
       volume_group     => $volume_group,
     }
+    if $setup_test_volume {
+      class {'::cinder::setup_test_volume':}
+    }
+  } else {
+    warning("Unsupported volume driver: ${volume_driver}, make sure you are configuring this yourself")
   }
-  if $setup_test_volume {
-    class {'::cinder::setup_test_volume':}
-   }
 }
