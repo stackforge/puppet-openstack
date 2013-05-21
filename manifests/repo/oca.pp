0774ab932e85057fcc9c87701c430f070bcc81c7
Adam Compton <acompton@quantcast.com>
2013-05-21 11:51:58 -0700

    Adding an "openstack::repo" class which automatically sets up repos
    Tested with the following combinations:
    
    - RHEL-alikes, OpenStack Folsom
    - RHEL-alikes, OpenStack Grizzly
    - Fedora 18, OpenStack Grizzly (F18 includes folsom)
    - Ubuntu 12.04+, OpenStack Folsom
    - Ubuntu 12.04+, OpenStack Grizzly
    
    Change-Id: I737da83d138695a178aaf3fa711a6e08a678adde

---
+++ manifests/repo/oca.pp
@@ -0,0 +1,15 @@
+# OpenStack Cloud Archive repo (supports either Folsom or Grizzly)
+class openstack::repo::oca(
+  $release = 'grizzly'
+) {
+  include apt::update
+
+  apt::source { 'openstack-cloud-archive':
+    location          => 'http://ubuntu-cloud.archive.canonical.com/ubuntu',
+    release           => "precise-updates/${release}",
+    repos             => 'main',
+    required_packages => 'ubuntu-cloud-keyring',
+  }
+
+  Exec['apt_update'] -> Package<||>
+}
