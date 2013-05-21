190e3e0ea75e537808fa0425b0974d6a14edff6e
Adam Compton <acompton@quantcast.com>
2013-05-21 11:51:58 -0700

    Adding an "openstack::repo" class which automatically sets up repos
    Tested with the following combinations:
    
    - RHEL-alikes, OpenStack Folsom
    - RHEL-alikes, OpenStack Grizzly
    - Fedora 18, OpenStack Grizzly (F18 includes folsom)
    - Ubuntu 12.04+, OpenStack Folsom
    - Ubuntu 12.04+, OpenStack Grizzly
    
    Change-Id: I737da83d138695a178aaf3fa711a6e08a678adde

---
+++ manifests/repo/rdo.pp
@@ -0,0 +1,31 @@
+# RDO repo (supports Grizzly on both RHEL-alikes and Fedora, requires EPEL)
+class openstack::repo::rdo {
+  include openstack::repo::epel
+
+  if $::osfamily == 'RedHat' {
+    $dist = $::operatingsystem ? {
+      'CentOS' => 'epel',
+      'Fedora' => 'fedora',
+    }
+    # $lsbmajdistrelease is only available with redhat-lsb installed
+    $osver = regsubst($::operatingsystemrelease, '(\d+)\..*', '\1')
+
+    yumrepo { 'rdo-release':
+      baseurl  => "http://repos.fedorapeople.org/repos/openstack/openstack-grizzly/${dist}-${osver}/",
+      descr    => 'OpenStack Grizzly Repository',
+      enabled  => 1,
+      gpgcheck => 1,
+      gpgkey   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-RDO-Grizzly',
+      priority => 98,
+      notify   => Exec['yum_refresh'],
+    }
+    file { '/etc/pki/rpm-gpg/RPM-GPG-KEY-RDO-Grizzly':
+      source => 'puppet:///modules/openstack/RPM-GPG-KEY-RDO-Grizzly',
+      owner  => root,
+      group  => root,
+      mode   => 644,
+      before => Yumrepo['rdo-release'],
+    }
+    Yumrepo['rdo-release'] -> Package<||>
+  }
+}
