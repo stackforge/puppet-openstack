fa2027e254f2ea61dcf800dff7bb0ae34f36bbcd
Adam Compton <acompton@quantcast.com>
2013-05-21 11:51:58 -0700

    Adding an "openstack::repo" class which automatically sets up repos
    Tested with the following combinations:
    
    - RHEL-alikes, OpenStack Folsom
    - RHEL-alikes, OpenStack Grizzly
    - Fedora 18, OpenStack Grizzly (F18 includes folsom)
    - Ubuntu 12.04+, OpenStack Folsom
    - Ubuntu 12.04+, OpenStack Grizzly
    
    Change-Id: I737da83d138695a178aaf3fa711a6e08a678adde

---
+++ manifests/repo/epel.pp
@@ -0,0 +1,23 @@
+# EPEL repo (RHEL-alikes only, _not_ Fedora)
+class openstack::repo::epel {
+  include openstack::repo::yum_refresh
+
+  if $::osfamily == 'RedHat' and $::operatingsystem != 'Fedora' {
+    yumrepo { 'epel':
+      mirrorlist     => 'https://mirrors.fedoraproject.org/metalink?repo=epel-6&arch=$basearch',
+      descr          => 'Extra Packages for Enterprise Linux 6 - $basearch',
+      enabled        => 1,
+      gpgcheck       => 1,
+      gpgkey         => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-6',
+      failovermethod => priority,
+      notify         => Exec['yum_refresh']
+    }
+    file { '/etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-6':
+      source => 'puppet:///modules/openstack/RPM-GPG-KEY-EPEL-6',
+      owner  => root,
+      group  => root,
+      mode   => 644,
+    }
+    Yumrepo['epel'] -> Package<||>
+  }
+}
