bb9ec20383d879c726f1135378b8c184e037d12c
Michael Chapman <michchap@cisco.com>
2013-06-05 17:21:25 +1000

    Add an openstack::client class for client nodes
    Add a class that can be used to create nodes that contain
    only the openstack client libraries.
    
    Also add ceilometer to fixtures so tests pass.
    
    Change-Id: I0b9175a582b42fac5cb9f9a10bd5bc877de8632d

---
+++ spec/classes/openstack_client_spec.rb
@@ -0,0 +1,47 @@
+require 'spec_helper'
+
+describe 'openstack::client' do
+
+  let :facts do
+    { :osfamily => 'Debian', :operatingsystem => 'Ubuntu' }
+  end
+
+  describe 'with default params' do
+    it { should include_class('ceilometer::client') }
+    it { should include_class('cinder::client') }
+    it { should include_class('glance::client') }
+    it { should include_class('keystone::client') }
+    it { should include_class('nova::client') }
+    it { should include_class('quantum::client') }
+  end
+
+  describe 'without ceilometer' do
+    let (:params) { {:ceilometer => false }}
+    it { should_not include_class('ceilometer::client') }
+  end
+
+  describe 'without cinder' do
+    let (:params) { {:cinder => false }}
+    it { should_not include_class('cinder::client') }
+  end
+
+  describe 'without glance' do
+    let (:params) { {:glance => false }}
+    it { should_not include_class('glance::client') }
+  end
+
+  describe 'without keystone' do
+    let (:params) { {:keystone => false }}
+    it { should_not include_class('keystone::client') }
+  end
+
+  describe 'without nova' do
+    let (:params) { {:nova => false }}
+    it { should_not include_class('nova::client') }
+  end
+
+  describe 'without quantum' do
+    let (:params) { {:quantum => false }}
+    it { should_not include_class('quantum::client') }
+  end
+end
