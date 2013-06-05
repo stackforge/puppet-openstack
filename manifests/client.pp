f6967ba6596bb8d8f74c5d2936c40dc8b2fb250b
Michael Chapman <michchap@cisco.com>
2013-06-05 17:21:25 +1000

    Add an openstack::client class for client nodes
    Add a class that can be used to create nodes that contain
    only the openstack client libraries.
    
    Also add ceilometer to fixtures so tests pass.
    
    Change-Id: I0b9175a582b42fac5cb9f9a10bd5bc877de8632d

---
+++ manifests/client.pp
@@ -0,0 +1,57 @@
+#
+# Installs only the OpenStack client libraries
+#
+# === Parameters
+#
+# [ceilometer]
+#   (optional) Install the Ceilometer client package
+#
+# [cinder]
+#   (optional) Install the Cinder client package
+#
+# [glance]
+#   (optional) Install the Glance client package
+#
+# [keystone]
+#   (optional) Install the Keystone client package
+#
+# [nova]
+#   (optional) Install the Nova client package
+#
+# [quantum]
+#   (optional) Install the Quantum client package
+#
+
+class openstack::client (
+  $ceilometer = true,
+  $cinder = true,
+  $glance = true,
+  $keystone = true,
+  $nova = true,
+  $quantum = true
+) {
+
+  if $ceilometer {
+    include 'ceilometer::client'
+  }
+
+  if $cinder {
+    include 'cinder::client'
+  }
+
+  if $glance {
+    include 'glance::client'
+  }
+
+  if $keystone {
+    include 'keystone::client'
+  }
+
+  if $nova {
+    include 'nova::client'
+  }
+
+  if $quantum {
+    include 'quantum::client'
+  }
+}
