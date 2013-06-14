296679e6abfb91277bc7ab7aa3ebe310d605e228
Simon Sellar <simon.sellar@ersa.edu.au>
2013-06-14 14:52:39 +0930

    Storage node now uses local zone variable name.
    Puppet 3 scoping changes mean that the exported resources cannot see
    the parent scope $swift_zone variable, and must instead use the $zone
    variable passed to the device_endpoint defined type.
    
    Change-Id: I9780065c535da713c6d1d4a156f5d688e0ab9c63
    Fixes: bug #1190459

--- manifests/swift/storage-node.pp
+++ manifests/swift/storage-node.pp
@@ -33,15 +33,15 @@ class openstack::swift::storage-node (
 
   define device_endpoint ($swift_local_net_ip, $zone, $weight) {
     @@ring_object_device { "${swift_local_net_ip}:6000/${name}":
-      zone   => $swift_zone,
+      zone   => $zone,
       weight => $weight,
     }
     @@ring_container_device { "${swift_local_net_ip}:6001/${name}":
-      zone   => $swift_zone,
+      zone   => $zone,
       weight => $weight,
     }
     @@ring_account_device { "${swift_local_net_ip}:6002/${name}":
-      zone   => $swift_zone,
+      zone   => $zone,
       weight => $weight,
     }
   }
