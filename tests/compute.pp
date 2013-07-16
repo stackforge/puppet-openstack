fcef8ce9f2d297930893ab91abb79d9346561238
William Van Hevelingen <blkperl@cat.pdx.edu>
2013-07-16 12:44:36 -0700

    (WIP) Fix smoke tests
    * Add missing params and remove deprecated params from tests
    * Fix classname changes
    * Add new smoketests for testfile and auth_file
    
    Change-Id: Ib25fcd6545aa704f64dd0709a75700480eaff684

--- tests/compute.pp
+++ tests/compute.pp
@@ -1,10 +1,12 @@
-class { 'openstack::nova::compute':
-  exported_resources   => false,
-  sql_connection       => 'mysql://foo:bar@192.168.1.1/nova',
+class { 'openstack::compute':
+  cinder_db_password   => 'password',
+  fixed_range          => '192.168.101.64/28',
   glance_api_servers   => '192.168.1.1:9292',
   internal_address     => $::ipaddress_eth1,
-  rabbit_password      => 'password',
-  nova_user_password   => 'password',
   libvirt_type         => 'qemu',
+  nova_db_password     => 'password',
+  nova_user_password   => 'password',
+  quantum              => false,
+  rabbit_password      => 'password',
   vncproxy_host        => '192.168.1.1',
 }
