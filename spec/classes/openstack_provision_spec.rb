e98e906d8c3bc70f7395ce92584e4e481909541f
Dan Bode <bodepd@gmail.com>
2013-07-22 16:08:10 -0700

    Add change_password_available param
    Add change_password_available parameter so that we can
    configure tempest to not run tests that require
    changing the VMs password.
    
    Change-Id: I98067790530ab153e1a316c4789fc1fb2ee96b79

--- spec/classes/openstack_provision_spec.rb
+++ spec/classes/openstack_provision_spec.rb
@@ -11,15 +11,17 @@ describe 'openstack::provision' do
   describe 'should be possible to override resize_available' do
     let :params do
       {
-        :configure_tempest => true,
-        :resize_available  => true,
-        :version_to_test   => 'stable/grizzly'
+        :configure_tempest         => true,
+        :resize_available          => true,
+        :change_password_available => true,
+        :version_to_test           => 'stable/grizzly'
       }
     end
 
     it { should contain_class('tempest').with(
-      :resize_available => true,
-      :version_to_test  => 'stable/grizzly'
+      :resize_available          => true,
+      :change_password_available => true,
+      :version_to_test           => 'stable/grizzly'
     ) }
 
   end
