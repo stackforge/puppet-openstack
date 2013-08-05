4b5f279856ead236dc3dad02351fb2e703d8d34a
Mathieu Gagné <mgagne@iweb.com>
2013-08-05 16:55:07 -0400

    Add ability to configure endpoint_type in openrc
    * Add the ability to configure the endpoint type used by the client
    * Refactor to use a template for the generation of openrc
    
    Change-Id: Id4b214373676488930be2066ad96ab3261ded909

--- manifests/auth_file.pp
+++ manifests/auth_file.pp
@@ -4,35 +4,26 @@
 # against a keystone server.
 #
 class openstack::auth_file(
-  $controller_node      = '127.0.0.1',
-  $keystone_admin_token = undef,
-  $admin_user           = 'admin',
-  $admin_password       = undef,
-  $admin_tenant         = 'admin',
-  $region_name          = 'RegionOne',
-  $use_no_cache         = true
+  $controller_node          = '127.0.0.1',
+  $keystone_admin_token     = undef,
+  $admin_user               = 'admin',
+  $admin_password           = undef,
+  $admin_tenant             = 'admin',
+  $region_name              = 'RegionOne',
+  $use_no_cache             = true,
+  $ceilometer_endpoint_type = 'publicURL',
+  $cinder_endpoint_type     = 'publicURL',
+  $glance_endpoint_type     = 'publicURL',
+  $heat_endpoint_type       = 'publicURL',
+  $keystone_endpoint_type   = 'publicURL',
+  $nova_endpoint_type       = 'publicURL',
+  $quantum_endpoint_type    = 'publicURL',
 ) {
 
-  if ($keystone_admin_token) {
-    file { '/root/openrc':
-        content =>
-    "
-    export OS_SERVICE_TOKEN=${keystone_admin_token}
-    export OS_SERVICE_ENDPOINT=http://${controller_node}:35357/v2.0/
-    "
-    }
-  } else {
-    file { '/root/openrc':
-      content =>
-    "
-    export OS_NO_CACHE=${use_no_cache}
-    export OS_TENANT_NAME=${admin_tenant}
-    export OS_USERNAME=${admin_user}
-    export OS_PASSWORD='${admin_password}'
-    export OS_AUTH_URL=\"http://${controller_node}:5000/v2.0/\"
-    export OS_AUTH_STRATEGY=keystone
-    export OS_REGION_NAME=${region_name}
-    "
-    }
+  file { '/root/openrc':
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0700',
+    content => template("${module_name}/openrc.erb")
   }
 }
