fca6910d986ff638d2e1ad2c33f96f8b3d6b4fdf
Matthew Black <matthew_black@cable.comcast.com>
2013-09-23 10:47:28 -0400

    Allow passing keystone_host to controller class
    The different components defaults to 127.0.0.1 for keystone
    for the controller class. This fix allows to override that for
    situations where the keystone service is not deployed on the
    controller.
    
    Change-Id: I982de99d6b96f09dc086f46f8db6acb830211cd6

--- manifests/controller.pp
+++ manifests/controller.pp
@@ -448,6 +448,8 @@ class openstack::controller (
     rabbit_virtual_host     => $rabbit_virtual_host,
     # Glance
     glance_api_servers      => $glance_api_servers,
+    # Keystone
+    keystone_host           => $keystone_host,
     # VNC
     vnc_enabled             => $vnc_enabled,
     vncproxy_host           => $vncproxy_host_real,
