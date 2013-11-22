a320e88254f612c6488fdadd17257464587e85a9
Jeffrey Cox <jefcox@indiana.edu>
2013-11-21 20:59:31 -0500

    Add parameter for custom Horizon local settings template
    Module stackforge/puppet-horizon uses puppet templating to create the
    /etc/openstack-horizon/local_settings.py configuration file. That module
    also allows the end user manifests of the module to specify a different template
    via the parameter local_settings_template.
    
    However, this module (stackforge/puppet-openstack) which uses the
    puppet-horizon module does not expose this parameter. So there is no way to apply a custom template.
    
    This change exposes the parameter and allows manifests using
    puppet-openstack/controller or puppet-openstack/all to pass in a custom local_settings template path.
    
    Change-Id: Ibfc8e4db01ddf04527ce7ff7513e5e1afdba4a9c

--- manifests/all.pp
+++ manifests/all.pp
@@ -45,6 +45,9 @@
 # [cache_server_ip]     local memcached instance ip
 # [cache_server_port]   local memcached instance port
 # [horizon]             (bool) is horizon installed. Defaults to: true
+# [local_settings_template]
+#    (optional) Location of template to use for local_settings.py generation.
+#
 # [neutron]             (bool) is neutron installed
 # [network_vlan_ranges] array of vlan_start:vlan_stop groups
 # [bridge_mappings]     array of physical_newtork:l2_start:l2end groups
@@ -185,6 +188,7 @@ class openstack::all (
   $cache_server_ip         = '127.0.0.1',
   $cache_server_port       = '11211',
   $horizon_app_links       = undef,
+  $local_settings_template = undef,
   # VNC
   $vnc_enabled             = true,
   $vncproxy_host           = false,
@@ -548,10 +552,11 @@ class openstack::all (
   ######## Horizon ########
   if ($horizon) {
     class { 'openstack::horizon':
-      secret_key        => $secret_key,
-      cache_server_ip   => $cache_server_ip,
-      cache_server_port => $cache_server_port,
-      horizon_app_links => $horizon_app_links,
+      secret_key              => $secret_key,
+      cache_server_ip         => $cache_server_ip,
+      cache_server_port       => $cache_server_port,
+      horizon_app_links       => $horizon_app_links,
+      local_settings_template => $local_settings_template,
     }
   }
 }
