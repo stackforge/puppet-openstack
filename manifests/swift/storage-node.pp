ffbe11e45fa69c466cf853a7bf97b8b222e240fe
cbaesema <cbaeseman@gmail.com>
2013-12-17 13:47:25 -0600

    Allow running of proxy and storage on the same node.
    This adds conditionals to support a installation
    in which a user desires to run both swift proxy and
    storage on the same node. It also makes it possible to
    disable ring sync if it is not desired or needed.
    
    Change-Id: I60fb78e5d40605ab47f349d154737ef9bab641f4

--- manifests/swift/storage-node.pp
+++ manifests/swift/storage-node.pp
@@ -12,9 +12,11 @@ class openstack::swift::storage-node (
   $byte_size            = '1024',
 ) {
 
-  class { 'swift':
-    swift_hash_suffix => $swift_hash_suffix,
-    package_ensure    => $package_ensure,
+  if !defined(swift){
+    class { 'swift':
+      swift_hash_suffix => $swift_hash_suffix,
+      package_ensure    => $package_ensure,
+    }
   }
 
   case $storage_type {
@@ -49,7 +51,9 @@ class openstack::swift::storage-node (
   }
 
   # rsync rings from the ring server
-  swift::ringsync { ['account','container','object']:
-    ring_server => $ring_server,
+  if ($ring_server){
+    swift::ringsync { ['account','container','object']:
+      ring_server => $ring_server,
+    }
   }
 }
