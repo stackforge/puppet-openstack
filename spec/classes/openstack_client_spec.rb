800d01c6f9ae20357bafbe33666f24905b5d7bf9
Mathieu Gagné <mgagne@iweb.com>
2013-12-23 02:43:51 -0500

    Remove use of deprecated include_class matcher
    include_class is deprecated since rspec-puppet 1.0.0.
    contain_class should be used instead.
    
    Closes-bug: #1263617
    Change-Id: I63fed009811684de1441a7b9d43325415d1860f9

--- spec/classes/openstack_client_spec.rb
+++ spec/classes/openstack_client_spec.rb
@@ -7,41 +7,41 @@ describe 'openstack::client' do
   end
 
   describe 'with default params' do
-    it { should include_class('ceilometer::client') }
-    it { should include_class('cinder::client') }
-    it { should include_class('glance::client') }
-    it { should include_class('keystone::client') }
-    it { should include_class('nova::client') }
-    it { should include_class('neutron::client') }
+    it { should contain_class('ceilometer::client') }
+    it { should contain_class('cinder::client') }
+    it { should contain_class('glance::client') }
+    it { should contain_class('keystone::client') }
+    it { should contain_class('nova::client') }
+    it { should contain_class('neutron::client') }
   end
 
   describe 'without ceilometer' do
     let (:params) { {:ceilometer => false }}
-    it { should_not include_class('ceilometer::client') }
+    it { should_not contain_class('ceilometer::client') }
   end
 
   describe 'without cinder' do
     let (:params) { {:cinder => false }}
-    it { should_not include_class('cinder::client') }
+    it { should_not contain_class('cinder::client') }
   end
 
   describe 'without glance' do
     let (:params) { {:glance => false }}
-    it { should_not include_class('glance::client') }
+    it { should_not contain_class('glance::client') }
   end
 
   describe 'without keystone' do
     let (:params) { {:keystone => false }}
-    it { should_not include_class('keystone::client') }
+    it { should_not contain_class('keystone::client') }
   end
 
   describe 'without nova' do
     let (:params) { {:nova => false }}
-    it { should_not include_class('nova::client') }
+    it { should_not contain_class('nova::client') }
   end
 
   describe 'without neutron' do
     let (:params) { {:neutron => false }}
-    it { should_not include_class('neutron::client') }
+    it { should_not contain_class('neutron::client') }
   end
 end
