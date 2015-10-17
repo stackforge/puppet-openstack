b8f39c8a7638916c70de6006fb4150bcbf027e42
Monty Taylor <mordred@inaugust.com>
2015-10-17 16:04:20 -0400

    Retire stackforge/puppet-openstack

---
+++ README.rst
@@ -0,0 +1,7 @@
+This project is no longer maintained.
+
+The contents of this repository are still available in the Git source code
+management system. To see the contents of this repository before it reached
+its end of life, please check out the previous commit with 
+"git checkout HEAD^1".
+
--- spec/classes/openstack_test_file_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe 'openstack::test_file' do
-  it do
-    is_expected.to contain_file('/tmp/test_nova.sh').with_mode('0751')
-    is_expected.to_not contain_file('/tmp/test_nova.sh').with_content(/add-floating-ip/)
-    is_expected.to contain_file('/tmp/test_nova.sh').with_content(/floatingip-create/)
-  end
-end
